d183edad42115b5b607dcf4ed91beb7c82132c6a
Vadim Kantorov <vadimkantorov@gmail.com>
2022-10-23 22:38:15 +0200

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ function apply_geo_filter(search, update_filter_field = true)
         document.getElementById('filter_area').value = search;
     
     if(search != '')
-        document.querySelectorAll(`li:has(a:not([data-search=""]):not([data-search*="${search.toLowerCase()}"]))`).forEach(li => li.hidden = true);
+        document.querySelectorAll(`li:not([data-search=""]):not([data-search*="${search.toLowerCase()}"])`).forEach(li => li.hidden = true);
 }
 
 function choose_random_event()
