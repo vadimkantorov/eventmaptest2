4d9aafe6c1939f646894119516c1825576e2b723
Vadim Kantorov <vadimkantorov@gmail.com>
2022-10-31 12:15:00 +0200

    ...

---
+++ geocoder.py
@@ -0,0 +1,29 @@
+# https://www.wikidata.org/wiki/Wikidata:SPARQL_tutorial/en
+
+import json
+import urllib.parse
+import urllib.request
+
+url = 'https://query.wikidata.org/sparql'
+
+query = '''
+
+SELECT ?city ?cityLabel ?countryLabel ?iso ?population
+WHERE {
+  ?city wdt:P31 wd:Q1549591 . hint:Prior hint:runFirst true .
+  ?city wdt:P17 ?country .
+  ?country wdt:P297 ?iso .
+
+  	?city wdt:P1082 ?population .
+  FILTER (?population > 100000) .
+  
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+}
+ORDER BY DESC(?population)
+LIMIT 10
+'''
+
+print(url.replace('sparql', '#') + urllib.parse.quote(query))
+
+#j = json.loads(urllib.request.urlopen(url + '?' + urllib.parse.urlencode(dict(format = 'json', query = query))).read().decode('utf-8'))
+#print(j)
