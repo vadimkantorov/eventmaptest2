46049f3a95eda7bc12e87229da1ecac0e3354498
Vadim Kantorov <vadimkantorov@gmail.com>
2022-11-01 12:46:48 +0200

    ...

--- README.md
+++ README.md
@@ -14,3 +14,24 @@
 ./data/campaigns/ABC/ABC.jpg
 ./data/campaigns/ABC/photos/EFG.jpg
 ```
+
+## Main page
+`data/index/index.md`:
+```
+---
+title: (string) main page header
+logo: (string) absolute https-path or file path relative to data/index/ where images can be stored
+---
+(markdown) main page intro text
+```
+
+## Campaign pages
+`data/campaigns/2022-my-campaign-name-for-url/2022-my-campaign-name-for-url.md`:
+```
+---
+title: (string)
+date: 
+ - 2022-02-20
+ - 2022-11-28
+logo: 2022-stopmurderization.jpg
+```
--- geocoder.py
+++ geocoder.py
@@ -1,32 +1,36 @@
 # https://www.wikidata.org/wiki/Wikidata:SPARQL_tutorial/en
+# https://github.com/OSMNames/OSMNames
+# https://osmnames.org/download/
 
 import json
 import urllib.parse
 import urllib.request
 
+output_path = 'geocoder.json'
+
 url = 'https://query.wikidata.org/sparql'
 
 query = '''
 
-SELECT ?city ?cityLabel ?countryLabel ?iso ?population
+SELECT ?city ?cityLabel ?countryLabel ?iso ?population ?gps
 WHERE {
-  ?city wdt:P31 wd:Q1549591 . hint:Prior hint:runFirst true .
-  ?city wdt:P17 ?country .
-  ?country wdt:P297 ?iso .
-
-  	?city wdt:P1082 ?population .
-  FILTER (?population > 100000) .
-  
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+    ?city wdt:P31 wd:Q1549591 . hint:Prior hint:runFirst true .
+    ?city wdt:P17 ?country .
+    ?city wdt:P1082 ?population .
+    ?city wdt:P625 ?gps .
+    ?country wdt:P297 ?iso .
+    
+    FILTER (?population > 100000) .
+    
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
 }
 ORDER BY DESC(?population)
-LIMIT 10
+LIMIT 5000
 '''
 
 print(url.replace('sparql', '#') + urllib.parse.quote(query))
+print(output_path)
 
 j = json.loads(urllib.request.urlopen(url + '?' + urllib.parse.urlencode(dict(format = 'json', query = query))).read().decode('utf-8'))
-
-geocoder = {v['cityLabel']['value'] : v['population']['value'] for  v in j['results']['bindings']}
-
-json.dump(geocoder, open('geocoder.json', 'w'), indent = 2, sort_keys = True, ensure_ascii = False)
+geocoder = {v['cityLabel']['value'] : v['gps']['value'].replace('Point', '').strip('()').replace(' ', ',') for  v in j['results']['bindings']}
+json.dump(geocoder, open(output_path, 'w'), indent = 2, sort_keys = True, ensure_ascii = False)
